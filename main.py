e0bea4606a869cfe7db134282e080aeb161d1b0d
AllRedCat <leonismogabriel@gmail.com>
2024-05-05 00:19:07 -0300

    feat: add input for password and labels

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ root = Tk()
 frm = ttk.Frame(root, padding="100 30")
 frm.grid()
 
+user_label = ttk.Label(frm, text="Username").grid(column=0, row=0)
+
 username = StringVar()
-name = ttk.Entry(frm, textvariable=username)
-name.grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=2)
+name = ttk.Entry(frm, textvariable=username).grid(column=0, row=1)
+
+pass_label = ttk.Label(frm, text="Password").grid(column=0, row=2)
+
+password = StringVar()
+passw = ttk.Entry(frm, textvariable=password, show="*").grid(column=0, row=3)
+
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=99)
 
 root.mainloop()
 
 print(username.get())
+print(password.get())
